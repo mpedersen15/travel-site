d60e7ae02c5d2db7733f10aecbe201630baf127f
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-04-24 16:56:12 -0600

    added sprites gulp task

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,19 @@
+var 
+	gulp = require('gulp'),
+	svgSprite = require('gulp-svg-sprite')
+;
+
+var config = {
+	mode: {
+		css: {
+			
+		}
+	}
+}
+
+gulp.task('createSprite', function(){
+	return gulp.src('./app/assets/images/icons/**/*.svg')
+		.pipe(svgSprite(config))
+		.pipe(gulp.dest('./app/temp/sprite/'));
+	
+});
