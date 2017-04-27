14f862fbf74f8d5e182a69bc78297c1c0cb00e8c
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-04-26 22:26:40 -0600

    added basic JS OOP code in separate files (before Webpack)

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,6 @@
+var john = new Person('John Smith', 'blue');
+
+var jane = new Person('Jane Doe', 'pink');
+
+john.greet();
+jane.greet();
---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,7 @@
+function Person(name, favColor) {
+	this.name = name;
+	this.favColor = favColor;
+	this.greet = function(){
+		console.log('Hello, my name is ' + this.name + " and my favorite color is " + this.favColor);
+	}
+}
